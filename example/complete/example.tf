cd2d60ae5a8a72b30a97f7ca53aa34dc6893a6d4
rakeshclouddevops <rakeshgangwar.clouddrove.com>
2024-11-14 17:28:33 +0530

    updated sg

--- example/complete/example.tf
+++ example/complete/example.tf
@@ -43,35 +43,19 @@ module "security_group" {
       description = "Allow PostgreSQL traffic."
     }
   ]
-  
+
   new_sg_egress_rules_with_cidr_blocks = [
     {
       rule_count  = 1
       from_port   = 443
       protocol    = "tcp"
       to_port     = 443
-      cidr_blocks = ["0.0.0.0/0"]  # Allow only HTTPS outbound traffic
+      cidr_blocks = ["0.0.0.0/0"] # Allow only HTTPS outbound traffic
       description = "Allow outbound HTTPS traffic."
     }
   ]
-  
-
-
-
-  ## EGRESS Rules
-  # new_sg_egress_rules_with_cidr_blocks = [
-  #   {
-  #     rule_count  = 1
-  #     from_port   = 0
-  #     protocol    = "-1"
-  #     to_port     = 0
-  #     cidr_blocks = ["0.0.0.0/0"]
-  #     description = "Allow all outbound traffic."
-  #   }
-  # ]
 
 }
-
 
 # Define Subnet module
 module "public_subnet" {
