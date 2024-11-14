bdc72dd39d6074162018cf2534160281a74a4c60
rakeshclouddevops <rakesh.gangwar@clouddrove.com>
2024-11-14 16:50:54 +0530

    test

--- versions.tf
+++ versions.tf
@@ -2,22 +2,14 @@
 # ------------------------------------------------------------------------------
 # Versions
 # ------------------------------------------------------------------------------
-terraform {
-  required_version = ">= 1.4.6"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.1.0"
-}
-  }
-}
+
 terraform {
   required_version = ">= 1.6.6"
 
   required_providers {
 
     aws = {
-      source  = "hashicorp/aws"#
+      source  = "hashicorp/aws"
       version = ">= 5.31.0"
     }
     }
