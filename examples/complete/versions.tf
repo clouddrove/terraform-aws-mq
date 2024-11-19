a583f4e621f793db05068baad723e8581c4ccf33
rakeshclouddevops <rakesh.gangwar@clouddrove.com>
2024-11-19 18:20:38 +0530

    feat/main: Refactor Terraform configurations: Update variables, outputs, and examples (#6)
    * Refactor Terraform configurations: Update variables, outputs, and examples
    
    * feat: updated trigger
    
    ---------
    
    Co-authored-by: rakeshclouddevops <rakeshgangwar.clouddrove.com>
    Co-authored-by: Vishwajit Nagulkar <119565952+VishwajitNagulkar@users.noreply.github.com>

--- examples/complete/versions.tf
+++ examples/complete/versions.tf
@@ -1,11 +1,17 @@
+
+# ------------------------------------------------------------------------------
+# Versions
+# ------------------------------------------------------------------------------
+
 terraform {
   required_version = ">= 1.6.6"
 
   required_providers {
 
     aws = {
-      source  = "hashicorp/aws" #
+      source  = "hashicorp/aws"
       version = ">= 5.31.0"
     }
   }
 }
+
