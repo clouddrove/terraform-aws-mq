e635c4fe7936bfc157f365b46ce25f312fe848d9
Anmol Nagpal <anmolnagpal@users.noreply.github.com>
2026-03-20 07:37:31 -0400

    fix: correct required_version to >= 1.10.0

--- examples/complete/versions.tf
+++ examples/complete/versions.tf
@@ -4,7 +4,7 @@
 # ------------------------------------------------------------------------------
 
 terraform {
-  required_version = ">= 5.80.0"
+  required_version = ">= 1.10.0"
 
   required_providers {
 
